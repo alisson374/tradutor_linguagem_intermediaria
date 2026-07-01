6f410184325e46f83f8255cbf711ee79bebc26b4
Alisson <alissondosantos473@gmail.com>
2026-07-01 20:22:13 -0300

    test(test_tac): add firsts tests

---
+++ test_tac.py
@@ -0,0 +1,32 @@
+from tac import tac
+
+class TestTACGenerator:
+  def test_simple_assignment_shoud_paste(self):
+    # Example AST for testing
+    ast = [
+      ('assign', ('id', 'num'), ('num', 0))
+    ]
+
+    expected_tac = [
+      'num = 0'
+    ]
+
+    generated_tac = tac.generateTac(ast)
+
+    assert generated_tac == expected_tac, f"Expected {expected_tac}, but got {generated_tac}"
+
+  def test_if_statement_should_generate_correct_tac(self):
+    # Example AST for testing
+    ast = [
+      ('if', ('relop', '<', ('id', 'cont'), ('num', 5)), [('assign', ('id', 'num'), ('binop', '+', ('id', 'num'), ('id', 'cont2')))])
+    ]
+
+    expected_tac = [
+      'if cont < 5 goto L0',
+      'num = num + cont2',
+      'L0:'
+    ]
+
+    generated_tac = tac.generateTac(ast)
+
+    assert generated_tac == expected_tac, f"Expected {expected_tac}, but got {generated_tac}" 
