0da1a11a811ed0977d690d2bf2d294947a0c3e2b
Alisson <alissondosantos473@gmail.com>
2026-07-04 20:11:05 -0300

    test(test_tac): add first test to declaration and add comments to tests

--- test_tac.py
+++ test_tac.py
@@ -2,7 +2,8 @@ from tac import tac
 
 class TestTACGenerator:
   def test_simple_assignment_shoud_paste(self):
-    # Example AST for testing
+    # num = 0;
+    
     ast = [
       ('assign', ('id', 'num'), ('num', 0))
     ]
@@ -16,19 +17,34 @@ class TestTACGenerator:
     assert generated_tac == expected_tac, f"Expected {expected_tac}, but got {generated_tac}"
 
   def test_if_statement_should_generate_correct_tac(self):
-    # Example AST for testing
+    # if (cont < 5) {
+    #   num = num + cont2;
+    # }
+
     ast = [
       ('if', ('relop', '<', ('id', 'cont'), ('num', 5)), [('assign', ('id', 'num'), ('binop', '+', ('id', 'num'), ('id', 'cont2')))])
     ]
 
     expected_tac = [
-      'IF cont < 5 GOTO L0',
-      'num = num + cont2',
-      'GOTO L1',
-      'L0:',
-      'L1:'
+      'IF cont > 5 GOTO END1',
+      'T1 = num + cont2',
+      'num = T1',
+      'END1',
     ]
 
     generated_tac = tac.generateTac(ast)
 
     assert generated_tac == expected_tac, f"Expected {expected_tac}, but got {generated_tac}" 
+
+  def test_declaration_variable_should_generate_correct_tac(self):
+    # var
+    #   int cont;
+    
+    ast = [
+      ('declaration', [('int', ['var', 'cont'])])
+    ]
+
+
+    tac.generateTac(ast)
+
+    assert tac.variables[0] == ('int', ('var', 'cont')), f"Expected ('int', ('var', 'cont')), but got {tac.variables[0]}"
